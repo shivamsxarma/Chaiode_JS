de70150299529260de5006c7fe3f83855475a783
shivam sharma <shivamsxarma@gmail.com>
2026-02-10 15:39:43 +0530

    updated one

--- Basics.js/LVC.js
+++ Basics.js/LVC.js
@@ -1,1 +1,16 @@
-console.log("hellow shivam")
+  console.log("hellow shivam")
+  const accountID = 5511;
+  let accountEmail = "shivamsharma@gmail.com";
+  var accountPassword = "shivamsharma";
+  accountCity = "jaipur";
+
+//   accountID = 8888; not allowed
+accountEmail ="ss@gmail.com";
+accountPassword = "whyyyyyyshivam";
+  console.log(accountID);
+  accountCity = "ajmer";
+
+//   generally do not use var because it create problem in function and block scope  
+
+  console.table([accountID,accountEmail,accountPassword,accountCity])
+  
