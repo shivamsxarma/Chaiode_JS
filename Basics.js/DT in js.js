936a876ddc541b8b86bf959f13c3360546661bfa
shivam sharma <shivamsxarma@gmail.com>
2026-02-11 15:53:16 +0530

    updated one

---
+++ Basics.js/DT in js.js
@@ -0,0 +1,14 @@
+let Name = "shivam" ;
+let lastName = 'sharma';
+let age = 18;
+let isLoggedIn = false;
+let value 
+let password = null;
+let largeNumber = 100000*1000000;
+console.log(typeof largeNumber)
+console.log(typeof lastName)
+console.log(typeof Name )
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof value)
+console.log(typeof password)
