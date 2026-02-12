ef60dd650e9eef05891f6a4829d91144a59a0305
shivam sharma <shivamsxarma@gmail.com>
2026-02-12 21:43:50 +0530

    new update 12/2/2026

--- Basics.js/DT in js.js
+++ Basics.js/DT in js.js
@@ -4,7 +4,7 @@ let age = 18;
 let isLoggedIn = false;
 let value 
 let password = null;
-let largeNumber = 100000*1000000;
+let largeNumber = 123456789012345678901234567890n;
 console.log(typeof largeNumber)
 console.log(typeof lastName)
 console.log(typeof Name )
