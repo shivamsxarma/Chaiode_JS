5b08b0dbfe9cf788a794cedf4d0a34393e2fc188
shivam sharma <shivamsxarma@gmail.com>
2026-03-17 16:34:20 +0530

    neww learnings

---
+++ Stack&Heap.js
@@ -0,0 +1,17 @@
+//stack(primitive) and heap memory(non-primitive)
+
+let myName = "shivam sharma";
+let anotherName = myName;
+anotherName="sharmaaaa"
+
+console.log(anotherName)
+console.log(myName)
+
+let userOne = {
+    email:"shivam@gmail.com",
+    upi : "shivam@ybl",
+}
+
+let userTwo="shiiiii@gmail.com"
+console.log(userOne.email)
+console.log(userTwo.email)
