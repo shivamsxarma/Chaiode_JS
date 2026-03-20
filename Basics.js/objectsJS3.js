3af0fee2029079efa20a0799e2f977c09d4a3f1f
shivam sharma <shivamsxarma@gmail.com>
2026-03-20 16:10:17 +0530

    hii new commit 3

---
+++ Basics.js/objectsJS3.js
@@ -0,0 +1,17 @@
+// const course = {
+//     coursename:"js in hindi",
+//     price:"999",
+//     courseInstructure:"shivam"
+// }
+
+// const{courseInstructure:instructor}=course
+// // console.log(courseInstructure)
+// console.log(instructor)
+
+// api 
+
+// {
+//     "name": "shivam",
+//     "courseName": "js in hindi",
+//     "price": "free",
+// }
