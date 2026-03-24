6a5d7f97d692ee53ed8743971fbb30763176a8a2
shivam sharma <shivamsxarma@gmail.com>
2026-03-24 14:03:05 +0530

    new update

--- Basics.js/functions.js
+++ Basics.js/functions.js
@@ -1,31 +1,56 @@
-function saymyname(){
-    console.log("s")
-console.log("h")
-console.log("i")
-console.log("v")
-console.log("a")
-console.log("m")
-}
+// function saymyname(){
+//     console.log("s")
+// console.log("h")
+// console.log("i")
+// console.log("v")
+// console.log("a")
+// console.log("m")
+// }
 
-saymyname();
+// const { use } = require("react")
 
-//2 numbers
-function addNumbers(Number1, Number2){
-    let result = Number1+Number2
-    return function hii(){
-      console.log("bss")
-    }
-}
-// const result = addNumbers(3,5)
-// result()
+// saymyname();
 
-// console.log("result:",result)
+// //2 numbers
+// function addNumbers(Number1, Number2){
+//     let result = Number1+Number2
+//     return function hii(){
+//       console.log("bss")
+//     }
+// }
+// // const result = addNumbers(3,5) 
+// // result()
 
-function loginUserMessage (username = "sam"){
-    if (!username ){
- console.log("Enter a valid username")
- return
-    }
-    return `${username} just logged in`
-}
-console.log( loginUserMessage("shivam"))
+// // console.log("result:",result)
+
+// function loginUserMessage (username = "sam"){
+//     if (!username ){
+//  console.log("Enter a valid username")
+//  return
+//     }
+//     return `${username} just logged in`
+// }
+// console.log( loginUserMessage("shivam")) 
+// console.log(   )
+
+// function calucalateCartPrice(val1,val2,num1){
+    // return val1,val2,num1  // this will not work
+    // return [val1,val2,num1]
+// return {val1 , 
+//     val2 ,
+//      num1};
+
+// }
+// console.log(calucalateCartPrice(200,400,500))
+// calucalateCartPrice()
+
+// const user = {
+//     username:"pen",
+//     price:199,
+// }
+
+// function handleobj(anyobject){
+//     console.log(`username is ${anyobject.username} and price is ${anyobject.price} `)
+// }
+
+// handleobj(user)
