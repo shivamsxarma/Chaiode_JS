6fe009bc63a8334abdba21a14a1aed6c4688f2fa
shivam sharma <shivamsxarma@gmail.com>
2026-03-25 13:44:15 +0530

    new update

---
+++ ControlFlow/switchCase.js
@@ -0,0 +1,35 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "april" ;
+
+switch (month) {
+    case 1 :
+        console.log("janursry");
+        
+        break;
+    case 2 :
+        console.log("feb");
+        
+        break;
+    case 3 :
+        console.log("march");
+        
+        break;
+    case "april"  :
+        console.log("april");
+        
+        break;
+
+
+    default:
+        console.log("default value");
+        
+        break;
+}
---
+++ ControlFlow/truthy.js
@@ -0,0 +1,9 @@
+const userEmail = "shivam@gmail.com"
+
+if (userEmail) {
+    console.log("Got User Email");
+    
+}else{
+    console.log("Dont get Email");
+    
+}
